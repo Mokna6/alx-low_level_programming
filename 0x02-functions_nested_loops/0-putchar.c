b472bfaea35d5002dfb1c91bfb254681f429e39b
mohammed <eng.saber.ms@gmail.com>
2023-09-12 15:31:01 -0700

    Rwa

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main -Entry point
+ *
+ * description: [rints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "-putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		-putchar(str[ch]);
+	-putchar('\n');
+
+	return (0);
+
+}
